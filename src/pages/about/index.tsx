7220ee6466cccf27c6fde0efa3dd5878fd397037
kim mari <53479112+mariHidream@users.noreply.github.com>
2024-06-28 16:21:05 +0900

    d

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage: React.FC = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>This is the About page content.</p>
+    </div>
+  );
+};
+
+export default AboutPage;
