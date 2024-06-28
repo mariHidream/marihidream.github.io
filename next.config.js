e12424b2a42d3922910fa0cd661bfecf61f27d5e
kim mari <53479112+mariHidream@users.noreply.github.com>
2024-06-28 15:36:04 +0900

    s

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
- 
+  output: 'export',
   pageExtensions: ['jsx', 'js', 'ts', 'tsx'], // 페이지 파일의 확장자 설정
   webpack(config, options) {
     config.resolve.modules.push(__dirname); // 절대 경로 설정을 위해 현재 디렉토리를 추가합니다.
@@ -11,8 +11,7 @@ module.exports = {
     // ESLint 설정을 추가합니다.
     dirs: ['src/pages'], // src/pages 디렉토리에서 ESLint 검사를 수행하도록 설정합니다.
   },
-  output: 'export',
-  assetPrefix: isProd ? 'https://marihidream.github.io' : '',
   trailingSlash: true,
-  reactStrictMode: true
+  reactStrictMode: true,
+  assetPrefix: isProd ? 'https://marihidream.github.io' : ''
 };
